483293a863b6a23eb6cef96c83ac7e2a880eea7d
gcadigwe@gmail.com <gcadigwe@gmail.com>
2024-11-06 20:36:03 +0100

    dynamic array implementation

--- dynamic-array-implementation/main.cpp
+++ dynamic-array-implementation/main.cpp
@@ -6,9 +6,119 @@
 //
 
 #include <iostream>
+#include <optional>
+
+class DynamicArray {
+public:
+    int arraycapacity;
+    int* dynamicArray;
+    
+    DynamicArray(int capacity) {
+        
+        arraycapacity = capacity;
+        
+        dynamicArray = new int[capacity];
+        
+        for(int i = 0; i < capacity; i++) {
+            dynamicArray[i] = 0;
+        }
+        
+    }
+    
+    ~DynamicArray() {
+        delete [] dynamicArray;
+    }
+    
+    void insert(int value, std::optional<int>index = std::nullopt) {
+       
+        if(index){
+            int i = index.value();
+            if(i < arraycapacity && i >= 0){
+                dynamicArray[i] = value;
+            }
+        }else{
+            
+            int* newArray = new int[arraycapacity+1];
+            
+            for(int j = 0; j < arraycapacity+1; j++ ){
+                
+                if(j == arraycapacity){
+                    newArray[j] = value;
+                    
+                }else{
+                    newArray[j] = dynamicArray[j];
+                }
+                
+            }
+            
+            delete[] dynamicArray;
+            
+            dynamicArray = newArray;
+            
+            arraycapacity += 1;
+ 
+        }
+    }
+    
+    void print() const {
+        for (int i = 0; i < arraycapacity; i++) {
+            std::cout << dynamicArray[i] << " ";
+        }
+        
+        std::cout << std::endl;
+    }
+    
+    void remove(int index) {
+        if(index < 0 || index >= arraycapacity) {
+            
+            std::cout << "Index out of bounds." << std::endl;
+            
+            return;
+            
+        }
+        
+        int* newArray = new int[arraycapacity-1];
+        
+        for(int i = 0, j = 0; i < arraycapacity; i++){
+            
+            if(i != index) {
+               
+                newArray[j] = dynamicArray[i];
+                j++;
+            }
+            
+        }
+        
+        delete[] dynamicArray;
+        
+        dynamicArray = newArray;
+        arraycapacity -= 1;
+    }
+    
+    std::optional<int> get(int index) const {
+        
+        if(index < 0 || index >= arraycapacity) {
+            return std::nullopt;
+        }
+        
+        return dynamicArray[index];
+    }
+    
+    
+    int size() const {
+        
+        return arraycapacity;
+        
+    }
+    
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    DynamicArray arr(5);
+    std::cout << arr.size() << std::endl;
+    arr.insert(10);
+    std::cout << arr.size() << std::endl;
+    arr.print();
     return 0;
 }
